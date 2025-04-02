46c638328ea2c3e2b0233e5d607b13ac70d1e631
KanishkSingh0801 <kanishksinghjatav@gmail.com>
2025-04-02 11:48:34 +0530

    Patch Request

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -4,12 +4,16 @@ const port = 8080;
 const path = require("path");
 const {v4 : uuidv4} = require('uuid');
 uuidv4();
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname, "public")));
+
 
 
 let posts = [
@@ -71,8 +75,17 @@ app.patch("/posts/:id", (req,res) => {
 app.get("/posts/:id/edit", (req,res) => {
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
+    if (!post) {
+        return res.status(404).send("<h1>Post not found</h1>");
+    }
     res.render("edit.ejs", {post});
 });
+
+app.delete("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+})
 
 app.set(express.static(path.join(__dirname, "public")));
 app.listen(port, () => {
